4b1c484802b25e422f9923269bf217df1181d4a9
jk_lama <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>
2024-03-11 07:53:29 +0545

    added upto about and image still not working

--- dist/js/main.js
+++ dist/js/main.js
@@ -3,30 +3,26 @@ const hamburger = document.querySelector('.menu-btn__burger')
 let showMenu = false
 const nav = document.querySelector('.nav')
 const menuNav = document.querySelector('.menu-nav')
-const navItems = document.querySelectorAll('.menu-nav_item')
-const click = document.querySelector('.clickme')
+const navItems = document.querySelectorAll('.menu-nav__item')
 
-click.onClick = () => {
+menuBtn.addEventListener('click', () => {
   console.log('clicked')
-}
-
-menuBtn.onClick = () => {
   toggleMenu()
-}
+})
 
 function toggleMenu() {
   if (!showMenu) {
     hamburger.classList.add('open')
     nav.classList.add('open')
     menuNav.classList.add('open')
-    // navItems.forEach(item => item.classList.add('open'));
+    navItems.forEach((item) => item.classList.add('open'))
     console.log(showMenu)
     showMenu = true
   } else {
     hamburger.classList.remove('open')
     nav.classList.remove('open')
     menuNav.classList.remove('open')
-    // navItems.forEach(item => item.classList.remove('open'));
+    navItems.forEach((item) => item.classList.remove('open'))
     showMenu = false
   }
 }
